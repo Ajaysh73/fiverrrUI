d423718107f481bac5c5bfd4bda7567b04e4f3a0
Ajay Sharma <ajaysh73@gmail.com>
2023-04-18 13:44:39 -0400

    Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,167 @@
-function App() {
+import Navbar from './components/navbar/Navbar';
 
+function App() {
   return (
     <div>
-      Hello world!
+      <Navbar />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nisi
+        quisquam tenetur ex, doloremque rem? At perferendis cumque, voluptatum
+        repellat nihil dolorum minus non, pariatur consequuntur rerum quaerat
+        odio maxime.
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
