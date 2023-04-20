07e8c94bf62bfb8ae8739d0c86969fe6c8f156f9
Ajay Sharma <ajaysh73@gmail.com>
2023-04-20 14:19:40 -0400

    trustedBy component added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
+
 import './Home.scss';
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Featured />
+      <TrustedBy />
+    </div>
+  );
 };
 
 export default Home;
