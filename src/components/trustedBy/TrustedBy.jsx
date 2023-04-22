602c3a4d4d5d2ed2d2f632811aff1090e4949f03
Ajay Sharma <ajaysh73@gmail.com>
2023-04-22 14:22:39 -0400

    added category card, slide and couple of features in Home.jsx

--- src/components/trustedBy/TrustedBy.jsx
+++ src/components/trustedBy/TrustedBy.jsx
@@ -3,7 +3,6 @@ import './TrustedBy.scss';
 const TrustedBy = () => {
   return (
     <div className='trustedBy'>
-      {' '}
       <div className='container'>
         <span>Trusted by:</span>
         <img
